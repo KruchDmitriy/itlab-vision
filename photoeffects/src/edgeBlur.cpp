7057469663f1dc9e4078154d8515860c0edd9d80
KruchDmitriy <kruch.dmitriy@gmail.com>
2014-10-04 00:19:39 +0400

    another yet optimization

--- photoeffects/src/edgeBlur.cpp
+++ photoeffects/src/edgeBlur.cpp
@@ -1,5 +1,5 @@
 #include <opencv2/core/core.hpp>
-#include <opencv2/core/internal.hpp>
+#include <opencv2/core/utility.hpp>
 #include "photoeffects.hpp"
 
 using namespace cv;
@@ -7,30 +7,31 @@ using namespace cv;
 namespace
 {
 
-class edgeBlurInvoker
+class edgeBlurInvoker :public ParallelLoopBody
 {
-    public:
-
-    edgeBlurInvoker(const Mat& src, 
-                    const Mat& imgBoxFilt, 
-                    const Mat& mask, Mat& dst, 
-                    int indentTop, 
+public:
+    edgeBlurInvoker(const Mat& src,
+                    const Mat& boxFilt,
+                    Mat& dst,
+                    int indentTop,
                     int indentLeft)
     :   src_(src),
+        boxFilt_(boxFilt),
         dst_(dst),
-        imgBoxFilt_(imgBoxFilt),
-        mask_(mask),
         cols_(src.cols),
         indentTop_(indentTop),
         indentLeft_(indentLeft) {}
 
-    void operator()(const BlockedRange& rowsRange) const
+    void operator()(const Range& range) const
     {
-        Mat srcStripe = src_.rowRange(rowsRange.begin(), rowsRange.end());
-        Mat boxStripe = imgBoxFilt_.rowRange(rowsRange.begin(), 
-                                            rowsRange.end());
-        Mat dstStripe = dst_.rowRange(rowsRange.begin(), rowsRange.end());
-        Mat maskStripe = mask_.rowRange(rowsRange.begin(), rowsRange.end());
+        float halfWidth = src_.cols / 2.0f;
+        float halfHeight = src_.rows / 2.0f;
+        float a = (halfWidth - indentLeft_) * (halfWidth - indentLeft_);
+        float b = (halfHeight - indentTop_) * (halfHeight - indentTop_);
+
+        Mat srcStripe = src_.rowRange(range.start, range.end);
+        Mat boxStripe = boxFilt_.rowRange(range.start, range.end);
+        Mat dstStripe = dst_.rowRange(range.start, range.end);
 
         int rows = srcStripe.rows;
         for (int i = 0; i < rows; i++)
@@ -38,24 +39,26 @@ class edgeBlurInvoker
             uchar* row = (uchar*)srcStripe.row(i).data;
             uchar* boxRow = (uchar*)boxStripe.row(i).data;
             uchar* dstRow = (uchar*)dstStripe.row(i).data;
-            float* maskRow = (float*)maskStripe.row(i).data;
+            float y_part = (halfHeight - (i + range.start)) *
+                           (halfHeight - (i + range.start)) / b;
 
             for (int j = 0; j < 3 * cols_; j += 3)
             {
-                dstRow[j] = boxRow[j] * maskRow[j / 3] 
-                            + row[j] * (1.0f - maskRow[j / 3]);
-                dstRow[j + 1] = boxRow[j + 1] * maskRow[j / 3] 
-                                + row[j + 1] * (1.0f - maskRow[j / 3]);
-                dstRow[j + 2] = boxRow[j + 2] * maskRow[j / 3]
-                                + row[j + 2] * (1.0f - maskRow[j / 3]);
+                float maskEl = min(max(2.0f *
+                               ((halfWidth - j / 3) * (halfWidth - j / 3) / a +
+                               y_part - 0.5f), 0.0f), 1.0f);
+                float negMask = 1.0f - maskEl;
+
+                dstRow[j] = boxRow[j] * maskEl + row[j] * negMask;
+                dstRow[j + 1] = boxRow[j + 1] * maskEl + row[j + 1] * negMask;
+                dstRow[j + 2] = boxRow[j + 2] * maskEl + row[j + 2] * negMask;
             }
         }
     }
 
 private:
     const Mat& src_;
-    const Mat& imgBoxFilt_;
-    const Mat& mask_;
+    const Mat& boxFilt_;
     Mat& dst_;
     int indentTop_;
     int indentLeft_;
@@ -72,44 +75,19 @@ int edgeBlur(InputArray src, OutputArray dst, int indentTop, int indentLeft)
     CV_Assert(src.type() == CV_8UC3);
     dst.create(src.size(), src.type());
     Mat image = src.getMat(), outputImage = dst.getMat();
-    
+
     CV_Assert(indentTop >= 0 && indentTop <= (image.rows / 2 - 10));
     CV_Assert(indentLeft >= 0 && indentLeft <= (image.cols / 2 - 10));
 
-    float halfWidth = image.cols / 2.0f;
-    float halfHeight = image.rows / 2.0f;
-    float a = (halfWidth - indentLeft) * (halfWidth - indentLeft);
-    float b = (halfHeight - indentTop) * (halfHeight - indentTop);
-    float length = halfWidth * halfWidth / a 
-                + halfHeight * halfHeight / b - 1.0f;
-    Mat mask = Mat::zeros(image.size(), CV_32FC1);
+    Mat boxFilt(image.size(), CV_8UC3);
+    boxFilter(image, boxFilt, -1, Size(7, 7), Point(-1, -1),
+        true, BORDER_REPLICATE);
 
-    for (int i = 0; i < image.rows; i++)
-    {
-        for (int j = 0; j < image.cols; j++)
-        {
-            float magn = 
-                (halfWidth - i) * (halfWidth - i) / a + 
-                (halfHeight - i) * (halfHeight - i) / b - 1.0f;
-            if (magn > 0.0f)
-            {   
-                mask.at<float>(i,j) = magn / length;
-            }
-        }
-    }
-
-    int kSize = MAX(image.rows, image.cols) / 100;
-    Mat imgBoxFilt(image.size(), CV_8UC3);
-    boxFilter(image, imgBoxFilt, -1, 
-            Size(kSize, kSize), Point(-1, -1), 
-            true, BORDER_REPLICATE);
-
-    parallel_for(BlockedRange(0, image.rows), 
-        edgeBlurInvoker(image, 
-                        imgBoxFilt, 
-                        mask, 
-                        outputImage, 
-                        indentTop, 
-                        indentLeft));
+    parallel_for_(Range(0, image.rows),
+                  edgeBlurInvoker(image,
+                                  boxFilt,
+                                  outputImage,
+                                  indentTop,
+                                  indentLeft));
     return 0;
 }
